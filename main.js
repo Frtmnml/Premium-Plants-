c4bc5d6f45332d3e937df3f52befad3bf85bdafd
Joshua Herrera <42598687+Frtmnml@users.noreply.github.com>
2020-11-07 21:50:25 -0600

    Add files via upload
    Testing

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+}); 
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.articles' , { delay: 500});
+ScrollReveal().reveal('.articles' , { delay: 500});
+ScrollReveal().reveal('.article-banner-one' , { delay: 500});
